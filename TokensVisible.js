f728bc824d9666bbf6ab4f47142150454fb29638
David Zvekic <david.zvekic@gmail.com>
2021-03-12 12:07:07 -0500

    clean up indentation and formatting.

--- TokensVisible.js
+++ TokensVisible.js
@@ -320,14 +320,11 @@ Hooks.on('renderSceneControls', () => {
 	  if (activeFG) {
 		  document.querySelectorAll('#controls .control-tool.active:not(.toggle)').forEach(e=>e.style.setProperty('color',activeFG ));
       }
-	  	  
-});
+  }
+);
 
 
-// overrides of Token methods to implement the vision features of this module.
-
-Object.defineProperty(Token.prototype,'isVisible', 
-  {
+Object.defineProperty(Token.prototype,'isVisible', {
   	get()  
   	{
       if (!canvas.sight.tokenVision) return true; 
@@ -344,7 +341,8 @@ Object.defineProperty(Token.prototype,'isVisible',
 	  const tolerance = Math.min(this.w, this.h) / 4;
       return canvas.sight.testVisibility(this.center, {tolerance, object: this});
 	}
-  });
+  }
+);
 
 
 Token.prototype._isVisionSource = function () {
@@ -424,23 +422,24 @@ Token.prototype.setPosition=  async function ReplaceTokenSetPosition(x, y, {anim
 		  async function relativePan({dx,dy}){
 	 		  await canvas.animatePan({x:canvas.scene._viewPosition.x+dx, y: canvas.scene._viewPosition.y+dy})
 		  };
-		  let bounds = this.getBounds();
 		  
-	      let dx = 0;
-	      if (bounds.x< pad ) {
+		  
+		  let bounds = this.getBounds();
+		  let dx = 0;
+	  	  let dy = 0;
+
+	      if ( bounds.x < pad ) {
      		  dx = bounds.x- pad;
 	      } else
-	      if ((bounds.x+bounds.width) > (window.innerWidth - pad )) {
-		
-			  dx = (bounds.x+bounds.width) - (window.innerWidth - pad );
+	      if ( ( bounds.x+bounds.width ) > ( window.innerWidth - pad ) ) {
+		      dx = ( bounds.x + bounds.width ) - ( window.innerWidth - pad );
 	      };
-		   
-		  let dy = 0;
-          if(bounds.y<pad){
+	
+          if ( bounds.y < pad ){
 			  dy = bounds.y - pad;
 	      } else
-		  if (((bounds.y + bounds.height)> window.innerHeight - pad)) {
-               dy =  (bounds.y + bounds.height) - (window.innerHeight - pad );
+		  if ( ( bounds.y + bounds.height) > ( window.innerHeight - pad ) ) {
+               dy =  (bounds.y + bounds.height) - ( window.innerHeight - pad );
 		  } ;
 		  
 		  if  (dx || dy){
@@ -449,7 +448,7 @@ Token.prototype.setPosition=  async function ReplaceTokenSetPosition(x, y, {anim
       }
     }
     return this;
-  }
+};
   
   
 
@@ -516,14 +515,15 @@ Token.prototype.updateSource = function({defer=false, deleted=false, noUpdateFog
       canvas.sight.sources.delete(sourceId);
       if ( isVisionSource && !defer ) canvas.sight.refresh();
     }
-  }
+};
 
   
-tokensVisible.SightLayer = {_defaultcastRay:SightLayer._castRays, _defaultcomputeSight:SightLayer.computeSight};
+tokensVisible.SightLayer = {_defaultcastRay : SightLayer._castRays, 
+	                        _defaultcomputeSight : SightLayer.computeSight
+                           };
 
 tokensVisible.SightLayer._turboComputeSight = function (origin, radius, {angle=360, density=6, rotation=0, unrestricted=false}={}){
 
-   
      let key =  radius*34.34 +origin.x+(origin.y*7.654)+ canvas.dimensions.width*1.1 + canvas.dimensions.height*3.3 + density*11+ (unrestricted?33:-1) +angle*1.1 + rotation  +(canvas.walls.endpoints.length * 12.2);  
      let sightResult = tokensVisible.SightCache.get(key);
      if(sightResult!=undefined) {
@@ -532,7 +532,6 @@ tokensVisible.SightLayer._turboComputeSight = function (origin, radius, {angle=3
   
 	
 	sightResult = tokensVisible.SightLayer._defaultcomputeSight.call(this, origin, radius, {angle, density, rotation, unrestricted} );
-	
 	
     if(tokensVisible.SightCache.size>1000) tokensVisible.SightCache.delete(tokensVisible.SightCache.keys().next().value);
     tokensVisible.SightCache.set(key, sightResult);
@@ -544,211 +543,158 @@ tokensVisible.SightLayer._turboComputeSight = function (origin, radius, {angle=3
 tokensVisible.enableTurboSight = function() {
 	tokensVisible.SightCache=new Map();
 	SightLayer.computeSight=tokensVisible.SightLayer._turboComputeSight;
-	 
-	
-}
+};
 
 tokensVisible.disableTurboSight = function() {
 	delete tokensVisible.SightCache;
 	SightLayer.computeSight=tokensVisible.SightLayer._defaultcomputeSight;
-	
-}
+};
 
 
 tokensVisible.SightLayer._DI_castRays=function(x, y, distance, {density=4, endpoints, limitAngle=false, aMin, aMax}={}, cast, standardArray, rayAccuracy, rays,sorter) {
-
    
     const rOffset = 0.02;
-
-	
 
     // Enforce that all rays increase in angle from minimum towards maximum
     const rMin = limitAngle ? Ray.fromAngle(x, y, aMin, distance) : null;
     const rMax = limitAngle ? Ray.fromAngle(x, y, aMax, distance) : null;
    
 
-
-	if(endpoints.length || standardArray.length) {
-		const originaldensity = density;
+    if(endpoints.length || standardArray.length) {
+      const originaldensity = density;
 		
-		if(rayAccuracy<1.5)	density = density* (2/3) * rayAccuracy;  // density can never be worse than standard
-		if(density<Math.min(2,originaldensity))density=Math.min(2,originaldensity);  // dont want to make the quality too good/expensive. Use Supermode for that.
+	  if(rayAccuracy<1.5)	density = density* (2/3) * rayAccuracy;  // density can never be worse than standard
+	  if(density<Math.min(2,originaldensity))density=Math.min(2,originaldensity);  // dont want to make the quality too good/expensive. Use Supermode for that.
 		
-     // First prioritize rays which are cast directly at wall endpoints
-    for ( let e of endpoints ) {
-      const ray = Ray.fromAngle(x, y, Math.atan2(e[1]-y, e[0]-x), distance);
-      if ( limitAngle ) {
-        ray.angle = this._adjustRayAngle(aMin, ray.angle);  // Standardize the angle
-        if (!Number.between(ray.angle, aMin, aMax)) continue;
+      // First prioritize rays which are cast directly at wall endpoints
+      for ( let e of endpoints ) {
+        const ray = Ray.fromAngle(x, y, Math.atan2(e[1]-y, e[0]-x), distance);
+        if ( limitAngle ) {
+          ray.angle = this._adjustRayAngle(aMin, ray.angle);  // Standardize the angle
+          if (!Number.between(ray.angle, aMin, aMax)) continue;
+        }
+        cast(ray);
       }
-      cast(ray);
-    }
 
+      if (rayAccuracy<1.5) {
+        let varx = 0;
+        for(varx=0;varx<standardArray.length;varx++){
+          let currentAngle = standardArray[varx];
+          if(!limitAngle || (currentAngle>=rMin && currentAngle<=rMax)){
+            cast(Ray.fromAngle(x,y,currentAngle,distance),500);
+          }
+        }
+      }
 
-
-if (rayAccuracy<1.5) 
- {
-
-  let varx = 0;
-  for(varx=0;varx<standardArray.length;varx++){
-     let currentAngle = standardArray[varx];
-     if(!limitAngle || (currentAngle>=rMin && currentAngle<=rMax)){
-             cast(Ray.fromAngle(x,y,currentAngle,distance),500);
-     }
-  }
-}
-
-
-    const nr = rays.length;
-    for ( let i=0; i<nr; i++ ) {
-      const r = rays[i];
-//	  let newdistance = r.distance ;
-//      let nOffset = (rOffset * 12000) / r.distance; 
-
-//console.warn('noff',nOffset);
-      cast(r.shiftAngle(rOffset),1000);
-      cast(r.shiftAngle(-rOffset ),1000);
-
-    }
-
-    // Add additional limiting and central rays
-    if ( limitAngle ) {
-      const aCenter = aMin + ((aMax - aMin) / 2) + Math.PI;
-      const rCenter = Ray.fromAngle(x, y, aCenter, 0);
-      rCenter._isCenter = true;
-      cast(rMin);
-      cast(rCenter);
-      cast(rMax);
-    }
+      const nr = rays.length;
+      for ( let i=0; i<nr; i++ ) {
+        const r = rays[i];
  
-}
+        cast(r.shiftAngle(rOffset),1000);
+        cast(r.shiftAngle(-rOffset ),1000);
+      }
+
+      // Add additional limiting and central rays
+      if ( limitAngle ) {
+        const aCenter = aMin + ((aMax - aMin) / 2) + Math.PI;
+        const rCenter = Ray.fromAngle(x, y, aCenter, 0);
+        rCenter._isCenter = true;
+        cast(rMin);
+        cast(rCenter);
+        cast(rMax);
+      }
+ 
+    }
 
     function extraRays(density,tol=50){
-    // Add additional approximate rays to reach a desired radial density
-    if ( !!density ) {
+      // Add additional approximate rays to reach a desired radial density
+      if ( !!density ) {
 
-	  const rDensity = toRadians(density);
+	    const rDensity = toRadians(density);
         const nFill = Math.ceil((aMax - aMin) / rDensity);
  
-      for ( let a of Array.fromRange(nFill) ) {
-        cast(Ray.fromAngle(x, y, aMin + (a * rDensity), distance), tol);
+        for ( let a of Array.fromRange(nFill) ) {
+          cast(Ray.fromAngle(x, y, aMin + (a * rDensity), distance), tol);
+        }
       }
-    }
-
     };
 
 
     extraRays(density);
 
     // Sort rays counter-clockwise (increasing radians)
-    sorter(rays);
-	//console.warn('rays',rays.length);
+    // sorter is defined here via dependancy injection. 
+	sorter(rays);
     return rays;
-  };
+};
   
-  tokensVisible._setEnhancedCastRays=function()
-  {
+tokensVisible._setEnhancedCastRays = function() {
          
-          const standardArray = [-3.141592, -1.5707964,0,1.5707964];
+     const standardArray = [-3.141592, -1.5707964,0,1.5707964];
 
-    // Track rays and unique emission angles
+     // Track rays and unique emission angles
 
 	
-	      const sorter = (rays)=>{rays.sort((r1, r2) => r1.angle - r2.angle);};
-	  //     RayCache=new Map();
-		   if (tokensVisible.SightCache!=undefined) tokensVisible.SightCache=new Map();
-	       SightLayer._castRays=function(x, y, distance, {density=4, endpoints, limitAngle=false, aMin, aMax}={}) {
+     const sorter = (rays)=>{rays.sort((r1, r2) => r1.angle - r2.angle);};
+     if (tokensVisible.SightCache!=undefined) tokensVisible.SightCache=new Map();
+     SightLayer._castRays=function(x, y, distance, {density=4, endpoints, limitAngle=false, aMin, aMax}={}) {
 		 
-		
-	      // const key =  x.toString(16)+y.toString(16)+distance.toString(16)+limitAngle.toString()+aMin.toString()+aMax.toString();
-		  // we need a fast hash... if there is a collision (which is extremely unlikely), its even more unlikely that it will matter
-		  // because the placeable that is rendered incorrectly will probably be on separate places of the map
-	   //    const key =  x+(y*7.654)+distance+(limitAngle?1:0)+aMin+aMax;  
-	//	   let rays = RayCache.get(key);
-	let rays;
-	//	   if(rays!=undefined) {
-	//           // cache hit!
-	//		   return rays;
-	//	   }
-		   
-		   
-		   const rayAccuracy = canvas.sight.tokenVision?(canvas.scene._viewPosition.scale):5;
-	       const angles = new Set();
-	       rays = [];
-		  
-		   
-		   const cast = (ray, tol=50) => {
-   
-	            tol = tol *50 /(rayAccuracy * 10);
-	            let a = Math.round(ray.angle *tol  );// / tol;
-	            if ( angles.has(a) ) return;
-	            rays.push(ray);
-	            angles.add(a);
-	          };
+	     let rays = [];
+		 const rayAccuracy = canvas.sight.tokenVision?(canvas.scene._viewPosition.scale):5;
+	     const angles = new Set();
+		 const cast = (ray, tol=50) => {
+            tol = tol *50 /(rayAccuracy * 10);
+            let a = Math.round(ray.angle *tol  );// / tol;
+            if ( angles.has(a) ) return;
+            rays.push(ray);
+            angles.add(a);
+         };
 
-		   rays =tokensVisible.SightLayer._DI_castRays.call(this, x,y,distance,{density,endpoints,limitAngle,aMin,aMax},cast,standardArray, rayAccuracy, rays, sorter );
-	  //     if(RayCache.size>1000) RayCache.delete(RayCache.keys().next().value);
-	       
-	  //	   RayCache.set(key,rays);
-		   return rays;
-		   
-	   } 
+	     rays =tokensVisible.SightLayer._DI_castRays.call(this, x,y,distance,{density,endpoints,limitAngle,aMin,aMax},cast,standardArray, rayAccuracy, rays, sorter );
+	     return rays;
+	 }; 
 	   
 	   
-  }
+};
   
-  tokensVisible._setExtraEnhancedCastRays=function()
-  {
+tokensVisible._setExtraEnhancedCastRays = function() {
 	  
        const stubsorter = (rays)=>{ /* nothing to do because the rays are cast in sorted order in this mode */ };
        const realsorter = (rays)=>{rays.sort((r1, r2) => r1.angle - r2.angle);};
-	//   RayCache=new Map();
 	   if (tokensVisible.SightCache!=undefined) tokensVisible.SightCache=new Map();
 	  					    
 	   SightLayer._castRays=function(x, y, distance, {density, endpoints, limitAngle=false, aMin, aMax}={}){
 		 
 		   if (!canvas.sight.tokenVision) return tokensVisible.SightLayer._defaultcastRay.call(this,x,y,distance,{density:density+3,endpoints,limitAngle,aMin,aMax});
-	
-	//	const key =  x+(y*7.654)+distance+(limitAngle?1:0)+aMin+aMax;  
-	let rays;
-	//	   let rays = RayCache.get(key);
-	//	   if(rays!=undefined) {
-	//		   return rays;
-	//	   }
+ 	
+           let rays = [];
 
 		   let sorter;
-		   rays=[];
 		   const cast = (ray )=> rays.push(ray);
 		   const rayAccuracy = canvas.scene._viewPosition.scale;
-		  // console.warn('ray accuracy',rayAccuracy);
-           if (!limitAngle){
-           // endpoints and sorting are not needed for unlimited angle light or vision since we are casting rays in all directions anyway.
+       
+	       if (!limitAngle){
+               // endpoints and sorting are not needed for unlimited angle light or vision since we are casting rays in all directions anyway.
 		 	   endpoints=[]; 
 			   sorter=stubsorter;
 		   }
 		   else {
-           // endpoints and real sorting appear to be needed for limited angle light or vision to render correctly
+              // endpoints and real sorting needed for limited angle to render correctly
 		   	   sorter=realsorter;
 		   }
 		   density = rayAccuracy;
 		   
 		   rays = tokensVisible.SightLayer._DI_castRays.call(this,x,y,distance,{"density":Math.min(2.0,density),endpoints,limitAngle,aMin,aMax},cast,[],rayAccuracy,rays, sorter);
-	  //     if(RayCache.size>1000) RayCache.delete(RayCache.keys().next().value);
-	       
-	  //	   RayCache.set(key,rays);
-		   return rays;
+	  	   return rays;
 	   } 
-	   
 	    
-  };
+};
   
-tokensVisible._setStandardCastRays=function()
-  {
+  tokensVisible._setStandardCastRays = function() {
 	   if (tokensVisible.SightCache!=undefined) tokensVisible.SightCache=new Map();
        SightLayer._castRays=tokensVisible.SightLayer._defaultcastRay;
-	  
 	   	   
-  };
+};
   
 
    
